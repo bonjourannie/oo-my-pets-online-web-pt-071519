1ecccbcc79cbed005402d7f27d1f341480fbdc81
Annie Truex <artruex@gmail.com>
2019-08-16 21:48:25 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -51,6 +51,15 @@ class Owner
   end
   
   def sell_pets 
-    self.all.clear
+    pets = self.dogs + self.cats
+
+    pets.each do |pet|
+      pet.mood = "nervous"
+      pet.owner = nil
+    end
+  end
+
+  def list_pets
+    "I have #{self.dogs.count} dog(s), and #{self.cats.count} cat(s)."
   end
 end
